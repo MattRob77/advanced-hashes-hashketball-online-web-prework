bb2905e3a401551ff8fbb91b25e652844cc4c9a3
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-28 20:58:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -236,7 +236,8 @@ def player_with_longest_name
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
       if player[:player_name].length > long_name.length
-        
+        big_name = player[:player_name]
+
     
   
 end 
